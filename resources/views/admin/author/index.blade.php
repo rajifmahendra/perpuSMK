161c7428750cb4541865d3b23658a236b85cf45a
rajifmahendra <rajifmahendra10@gmail.com>
2019-12-11 20:58:31 +0700

    menyiapkan asset untuk tampil penulis

---
+++ resources/views/admin/author/index.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.templates.default')
+
+@section('content')
+<h1>Penulis</h1>
+<div class="box">
+  <table id="data" class="table table-bordered table-hover">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Nama</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Rajif</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+@endsection
--- resources/views/admin/templates/partials/script.blade.php
+++ resources/views/admin/templates/partials/script.blade.php
@@ -28,6 +28,9 @@
 <script src="assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
 <script src="assets/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- DataTables -->
+<script src="{{ asset('assets/bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="assets/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
